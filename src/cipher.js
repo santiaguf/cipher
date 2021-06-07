f5cb750f7cb8b9f6c93d5f3f5686064da121e74a
Santiago Bernal <exactlimon@gmail.com>
2021-06-07 17:24:24 -0500

    encode decode and tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,33 @@
 const cipher = {
   // ...
+  encode(offset, string) {
+    if (string === undefined || typeof offset !== 'number' || string === 0 || string === null) {
+      throw new TypeError('bad arguments');
+    }
+    let result = '';
+    for(let i=0; i<string.length; i++){
+      const Str2Asc = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+      const Asc2Str = String.fromCharCode(Str2Asc);
+      result = result.concat(Asc2Str);
+    }
+    return result;
+  },
+
+  decode(offset, string) {
+    if (string === undefined || typeof offset !== 'number' || string === 0 || string === null) {
+      throw new TypeError('bad arguments');
+    }
+    let result = '';
+    if(offset > 26){
+      offset = 26 - (offset - 26);
+    }
+    for(let i=0; i<string.length; i++){
+      const Str2Asc = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+      const Asc2Str = String.fromCharCode(Str2Asc);
+      result = result.concat(Asc2Str);
+    }
+    return result;
+  }
 };
 
 export default cipher;
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -65,14 +65,18 @@ describe('cipher', () => {
     });
 
     it('should throw TypeError when invoked with wrong argument types', () => {
-      expect(() => cipher.encode()).toThrow(TypeError);
-      expect(() => cipher.encode(0)).toThrow(TypeError);
-      expect(() => cipher.encode(null, [])).toThrow(TypeError);
-      expect(() => cipher.encode(0, 0)).toThrow(TypeError);
+      expect(() => cipher.decode()).toThrow(TypeError);
+      expect(() => cipher.decode(0)).toThrow(TypeError);
+      expect(() => cipher.decode(null, [])).toThrow(TypeError);
+      expect(() => cipher.decode(0, 0)).toThrow(TypeError);
     });
 
     it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
       expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    it('should return "KLMNOPQR" for "HIJKLMNO" with offset 3', () => {
+      expect(cipher.decode(3, 'HIJKLMNO')).toBe('KLMNOPQR');
     });
 
     //
