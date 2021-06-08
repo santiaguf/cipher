7a79bcc5c985f9c4fc8da39d3958e528a526f9a6
Santiago Bernal <exactlimon@gmail.com>
2021-06-07 19:27:57 -0500

    html in template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById('root').innerHTML = `
+  <div id="main-card">
+  <div id="card-title" class="main-text text"><h1>Lista de convocados Selección Colombia ⚽  🇨🇴</h1></div>
+  <div id="card-description" class="main-text text"><h2>El Director técnico de la selección Colombia de fútbol necesita enviar el listado de jugadores convocados para el siguiente partido, tu misión es cifrar los nombres de los jugadores,  para evitar que la lista se filtre en la prensa y los nombres se sepan antes de tiempo.</h2> </div>
+
+  <div id="cipher-container">
+    <div id="encode-container" class="box-container">
+      <div id="encode-text-container" >
+        <textarea id="encode-text" rows="4" placeholder="texto a cifrar"></textarea>
+      </div>
+      <div id="encode-button-container">
+        <button id="encode-button" class="button">Cifrar</button>
+      </div>
+      <div id="offset-container" class="text">
+        <span>desplazamiento/rotaciones</span>
+        <input id="offset" type="number" value="3">
+      </div>
+    </div>
+
+    <div id="decode-container" class="box-container">
+      <div id="decode-text-container" >
+        <textarea id="decode-text" rows="4" placeholder="texto a descifrar"></textarea>
+      </div>
+      <div id="decode-button-container">
+        <button id="decode-button" class="button">Descifrar</button>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <footer class="text"><h3>Cipher © 2021 - todos los derechos reservados - <a href="https://github.com/santiaguf/cipher" target="_blank">código fuente</a></h3></footer>
+`;
